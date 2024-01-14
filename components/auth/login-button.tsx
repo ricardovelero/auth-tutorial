1e040e10d78f8ee8f9d8692367426bf25be925bf
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-01-14 14:16:06 +0100

    Add modal mode

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import { LoginForm } from "./login-form";
 
 type LoginButtonProps = {
   children: React.ReactNode;
@@ -20,7 +22,14 @@ export default function LoginButton({
   };
 
   if (mode === "modal") {
-    return <span>TODO: implement modal</span>;
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+        <DialogContent className='p-0 w-auto bg-transparent'>
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    );
   }
   return (
     <span onClick={onClick} className='cursor-pointer'>
