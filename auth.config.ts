9835e5a405f4e2a56a3467111d0dafe2235901e8
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-01-15 10:10:49 +0100

    Fix email linking alowwance

--- auth.config.ts
+++ auth.config.ts
@@ -11,10 +11,12 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     Credentials({
       async authorize(credentials) {
