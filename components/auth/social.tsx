08c3dda816916d7db02f0981184a15a99dccc26f
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-01-15 09:33:17 +0100

    Add signin providers

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,15 +2,26 @@
 
 import { GitHubLogoIcon, LightningBoltIcon } from "@radix-ui/react-icons";
 import { Button } from "../ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 
 export function Social() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className='flex items-center w-full gap-x-2'>
       <Button
         size={"lg"}
         className='w-full'
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <LightningBoltIcon />
       </Button>
@@ -18,7 +29,7 @@ export function Social() {
         size={"lg"}
         className='w-full'
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <GitHubLogoIcon />
       </Button>
